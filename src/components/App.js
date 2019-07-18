363dc4db51818b873bfabb8146f7ff1862b7807d
SeetaRam-Yadav <SeetaRam_Yadav@external.mckinsey.com>
2019-07-18 07:34:54 +0530

    Intial setup for reactwith webpack dev middleware express server.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>React, webpack, webpack dev middileware, babel, eslint setup </h1>
+    </div>
+  )
+}
+
+export default App
