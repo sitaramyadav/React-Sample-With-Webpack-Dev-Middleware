431a654f4073222b164e65c77bb1d8dff8808a1d
SeetaRam-Yadav <SeetaRam_Yadav@external.mckinsey.com>
2019-10-24 15:36:48 +0530

    Fixing the webpack middle ware.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -13,7 +13,10 @@ module.exports = {
   },
   mode: 'development',
   target: 'web',
-  devtool: '#source-map',
+  devtool: 'inline-source-map',
+  devServer: {
+         contentBase: './dist',
+       },
   module: {
     rules: [
       {
