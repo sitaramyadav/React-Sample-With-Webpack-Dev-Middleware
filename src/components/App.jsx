bfdfce1e29fb3fc4423e46893d8b5071e84abb6f
Seetaram Yadav <=>
2020-09-21 17:48:28 +0530

    Fixing the you may need appropriate loader issue.

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return ( <div id = "app">
+        <h1> Flight Search Engine </h1> 
+        </div>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+import App from './components/App.jsx'
 
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app') // eslint-disable-line no-undef
+ReactDOM.render( <
+    App / > ,
+    document.getElementById('app') // eslint-disable-line no-undef
 )
 
-if(module.hot) // eslint-disable-line no-undef  
-  module.hot.accept() // eslint-disable-line no-undef  
-
+if (module.hot) // eslint-disable-line no-undef  
+    module.hot.accept() // eslint-disable-line no-undef
